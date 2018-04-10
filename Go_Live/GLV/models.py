8422d38801702f72490835fb656cc79b35fda5fa
kulkradhika0 <33073256+kulkradhika0@users.noreply.github.com>
2018-04-10 17:45:29 +0530

    Update models.py

--- Go_Live/GLV/models.py
+++ Go_Live/GLV/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class USER_MASTER(models.Model):
+	USER_ID = models.IntegerField(primary_key=True)
+	USERNAME = models.CharField(max_length = 40,null=True)
+	PASSCODE = models.IntegerField(null=True)
+	ORDER_ID = models.IntegerField(null=True)
